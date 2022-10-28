b3a02e1d5c4bdb62c2795e5f60a439f1fec867ee
Ramzi Al-Dorikhim <ramzik149@gmail.com>
2022-10-29 01:20:30 +0300

    Moved entity and application classes

--- core/src/main/java/liga/medical/medicalperson/core/MedicalPersonApplication.java
+++ core/src/main/java/liga/medical/medicalperson/core/MedicalPersonApplication.java
@@ -3,9 +3,11 @@ package liga.medical.medicalperson.core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"liga.medical.medicalperson", "liga.medical.common.service"})
+@ComponentScan(basePackages = {"liga.medical.medicalperson.core", "liga.medical.common.service"})
+@EnableAspectJAutoProxy
 public class MedicalPersonApplication {
     public static void main(String[] args) {
         SpringApplication.run(MedicalPersonApplication.class, args);
